6c8e26c89433da7ac0ce571a634fe1be7e88359e
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-03-10 13:01:56 -0500

    JavaScript demos.

---
+++ JavaScript/Objects/script.js
@@ -0,0 +1,10 @@
+let dog = "woof";
+let cat = "meow";
+let animals ={
+    dog,
+    cat
+}
+console.log(animals[0]);
+console.log(animals[dog]);
+console.log(animals['dog']);
+console.log(animals.dog);
