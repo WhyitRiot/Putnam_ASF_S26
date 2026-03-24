5bdef89f61d36444cd1e88302810528b77c75231
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-03-24 09:31:56 -0500

    Beginning react;

--- React/FirstComponent/firstcomponent/src/main.jsx
+++ React/FirstComponent/firstcomponent/src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App name={"Wyatt"} title={"The Reactonator"}/>
+    <App />
   </StrictMode>,
 )
