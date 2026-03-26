184042e5f3f369d42d6fdc12bd11666cf7d06691
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-03-26 10:57:29 -0500

    Track moved files.

--- React/FirstComponent/firstcomponent/TDD/word-counter/src/wordCounter.js
+++
@@ -1,9 +0,0 @@
-const wordCounter = (string)=>{
-    if (string){
-        words = string.replace(/[^A-Za-z0-9 ]/g, "").replace(/\s+/g, " ").split(" ")
-        return words.length;
-    }
-    return 0;
-}
-
-module.exports = wordCounter;
